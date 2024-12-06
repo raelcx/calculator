c6c2ba248d6a45f8c393aafef2397804848ea2de
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-12-06 15:53:57 -0300

    Add calculation logic

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 function sum(a, b) {
-
-};
+return a + b;
+}
 
 function subtract(a, b) {
-
-};
+return a - b;
+}
 
 function divide(a, b) {
-
-};
+return a / b;
+}
 
 function multiply(a, b) {
+return a * b;
+}
 
-};
+function operate(a, b, operator) {
+    switch(operator) {
+        case("+"):
+            return sum(a, b);
+            break;
+        case("-"):
+            return subtract(a, b);
+            break;
+        case("*"):
+            return multiply(a, b);
+            break;
+        case("/"):
+            return divide(a, b);
+            break;
+    }
+}
