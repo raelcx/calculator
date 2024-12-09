9153b855a33dc653f5981cff2cb3c555798b3c08
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-12-09 15:23:44 -0300

    Change display logic

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const display = document.getElementById("display");
+let number1;
+let number2;
+let operator;
+let showingResult = 0;
+let parentese = 'open';
 
 function sum(a, b) {
 return a + b;
@@ -34,71 +39,120 @@ function operate(a, b, operator) {
 }
 
 function updateDisplay(character) {
-    if (resultShown == 1) {
+    if (showingResult == 1) {
         display.textContent = '';
-        resultShown = 0
+        showingResult = 0
     }
+
+    let charToInsert;
 
     switch(character) {
         case("button1"):
-            display.textContent += '1';
+        charToInsert = '1';
             break;
+
         case("button2"):
-            display.textContent += '2';
+            charToInsert = '2';
             break;
+
         case("button3"):
-            display.textContent += '3';
+            charToInsert = '3';
             break;
+
         case("button4"):
-            display.textContent += '4';
+            charToInsert = '4';
             break;
+
         case("button5"):
-            display.textContent += '5';
+            charToInsert = '5';
             break;
+
         case("button6"):
-            display.textContent += '6';
+            charToInsert = '6';
             break;
+
         case("button7"):
-            display.textContent += '7';
+            charToInsert = '7';
             break;
+
         case("button8"):
-            display.textContent += '8';
+            charToInsert = '8';
             break;
+
         case("button9"):
-            display.textContent += '9';
+            charToInsert = '9';
             break;
+
         case("button0"):
-            display.textContent += '0';
+            charToInsert = '0';
             break;
+
         case("button-sum"):
-            display.textContent += ' + ';
+            charToInsert = ' + ';
             break;
+
         case("button-divide"):
-            display.textContent += ' / ';
+            charToInsert = ' / ';
             break;
+
         case("button-multiply"):
-            display.textContent += ' * ';
+            charToInsert = ' * ';
             break;
+
         case("button-subtract"):
-            display.textContent += ' - ';
+            charToInsert = ' - ';
             break;
+
         case("button-decimal"):
-            display.textContent += '.';
+            if (display.textContent.indexOf('.') == -1) {
+                charToInsert = '.';
+            }
             break;
+
+        case("button-parentese"):
+            let parenteseToInsert;
+            if (parentese == 'open') {
+                parenteseToInsert = '(';
+                parentese = 'close';
+            } else if (parentese == 'close') {
+                parenteseToInsert = ')';
+                parentese = 'open';
+            }
+            display.textContent += parenteseToInsert;
+            break;
+
+        case("button-delete"):
+            let deleted;
+            // Check if at left of next number there's a blank space and remove it if there is.
+            if (display.textContent.lastIndexOf(' ') == (display.textContent.length - 1)) {
+                deleted = display.textContent.slice(0, -2);
+            } else {
+                deleted = display.textContent.slice(0, -1);
+            }
+            display.textContent = deleted;
+            break;
+
         case("button-clear"):
             display.textContent = '';
             break;
+
         case("button-result"):
-            getOperation(display.textContent.split(' '));
+            getOperation(display.textContent);
             break;
     }
 
+    if (charToInsert && display.textContent.length < 32) {
+        display.textContent += charToInsert;
+        charToInsert = '';
+    }
 };
 
-function getOperation(displayValue) {
-    number1 = Number(displayValue[0]);
-    operator = displayValue[1];
-    number2 = Number(displayValue[2]);
+function getOperation(string) {
+    let cleaned = string.split(' ');
+
+    number1 = Number(cleaned[0]);
+    operator = cleaned[1];
+    number2 = Number(cleaned[2]);
 
     let result = operate(number1, number2, operator);
     displayResult(result);
@@ -106,14 +160,8 @@ function getOperation(displayValue) {
 
 function displayResult(result) {
     display.textContent = result;
-    resultShown = 1;
+    showingResult = 1;
 }
-
-let number1;
-let number2;
-let operator;
-let resultShown = 0;
-
 
 let calculator = document.getElementById("calculator");
 
